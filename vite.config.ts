9fa56d6599cd994801ddfa7269271afcfaeb3c7e
赵崇进 <1907784924@qq.com>
2026-08-20 10:35:06 +0800

    init personal blog site

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // !!!这里填你的github仓库名称，前后斜杠不能丢
-  base: '/my-vue-blog/'
-})
+  base: "/",
+});
